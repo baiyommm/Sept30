93f55914aad253c257ac811155617ad5d34f5af9
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-10-07 09:43:44 -0700

    Program to help a teacher analyze the grades of a class. With this program, there are functions to compute the average, highest number, lowest number, student that are above average, comparing students grading.

---
+++ Sept30/grade_analysis.cpp
@@ -0,0 +1,8 @@
+//
+//  grade_analysis.cpp
+//  Sept30
+//
+//  Created by Chatchapong Thuaypha on 10/2/24.
+//
+
+#include <stdio.h>
---
+++ Sept30/grade_analysis.h
@@ -0,0 +1,12 @@
+//
+//  grade_analysis.h
+//  Sept30
+//
+//  Created by Chatchapong Thuaypha on 10/2/24.
+//
+
+#ifndef grade_analysis_h
+#define grade_analysis_h
+
+
+#endif /* grade_analysis_h */
